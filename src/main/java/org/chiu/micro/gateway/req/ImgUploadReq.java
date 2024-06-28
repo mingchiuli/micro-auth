c0dc666c67ef020671bc41510cfd8da4f7a90a16
limingjiu <limingjiu@kanzhun.com>
2024-06-28 11:42:15 +0800

    fix

--- src/main/java/org/chiu/micro/gateway/req/ImgUploadReq.java
+++ src/main/java/org/chiu/micro/gateway/req/ImgUploadReq.java
@@ -2,10 +2,15 @@ package org.chiu.micro.gateway.req;
 
 import lombok.Data;
 
+/**
+ * @Author limingjiu
+ * @Date 2024/6/28 11:41
+ **/
 @Data
 public class ImgUploadReq {
-  
-    private byte[] data;
 
     private String fileName;
+
+    private byte[] data;
+
 }
