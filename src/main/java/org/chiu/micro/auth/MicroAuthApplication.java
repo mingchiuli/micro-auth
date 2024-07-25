8658dc8f3976b1b3dff7042d0138fc7f0dcc76fc
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-07-25 10:03:04 +0800

    Update MicroAuthApplication.java

--- src/main/java/org/chiu/micro/auth/MicroAuthApplication.java
+++ src/main/java/org/chiu/micro/auth/MicroAuthApplication.java
@@ -13,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MicroAuthApplication {
 
 		public static void main(String[] args) {
+				TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
 				SpringApplication.run(MicroAuthApplication.class, args);
 		}
 
